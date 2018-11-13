5144e8be60bc68f657747156d5d3f8ff760e4779
MunSoo <smboong@naver.com>
2018-11-13 22:44:43 +0900

    copy1

--- check.c
+++ check.c
@@ -1,59 +1,29 @@
 #include "myas.h"
 
-char* Reg[8] = {"%eax","%ecx","%edx","%ebx","%esp","%ebp","%esi","%edi"};
-char* Mem[8] = {"(%eax)","(%ecx)","(%edx)","(%ebx)","(%esp)","(%ebp)","(%esi)","(%edi)"};
-/*   */
+//check.c
+char* Reg[8] = { "%eax","%ecx","%edx","%ebx","%esp","%ebp","%esi","%edi"};
 
-int RegCheck(char *arg) {
-	int i=0;
-	for (i=0;i<8;i++){
-		if (strcmp(arg,Reg[i]) == 0)
-			return 2;
-	}
-	return 0;
-}
-int MemCheck(char *arg){
-	int i=0;
-	for (i=0;i<8;i++){
-		if (strstr(arg, Mem[i]) != NULL)
-			return 4;
-	}
-	if (arg[0]!='$' && strstr(arg,"0x") != NULL)
-		return 3;
-	return 0;
-}
-
-int is_valid(char *op, char *argA, char *argB)
+int Check_kind(char *Sen)
 {
-	int Memx = MemCheck(argA);
-	int Regx = RegCheck(argA);
-	int Regy = RegCheck(argB);
-	if (Memx == 3){
-		if(strcmp(argB, "%eax") ==0)
-			return 7;
-		else
-			return 0;
-	}
-	else if (Memx == 4){
-		if(RegCheck(argB))
-			return 6;
-		else
-			return 0;
-	}
-	else if (Regx){
-		if(strcmp(argA,"%eax") == 0){
-			if (MemCheck(argB))
-				return 8;
-		}
-		else if (Regy)
-			return 5;
-	}
-	else if(strstr(argA,"$0x") != NULL){
-		if(Regy)
-			return 9;
-		else
-			return 0;
-	}
+	int i=0;
+	if(Sen[0] == '$')
+		return 2;//imm
+	else if(Sen[0] == '(' || Sen[0] == ')' || Sen[0] == '0' || Sen[0] == '-')
+		return 3;//mem
+	else if (Sen[0] == '%')
+		return 4;//reg
 	else
+		return 0;//error
+}
+
+int is_valid(char *op, char *argF, char *argB)
+{
+	if((Check_kind(argF)==3) && (Check_kind(argB)==3))
 		return 0;
+	else if(Check_kind(argB)==2)
+		return 0;
+	else if((Check_kind(argF)==0) || (Check_kind(argB)==0))
+		return 0;
+	else
+		return 1;
 }
--- trans.c
+++ trans.c
@@ -1,60 +1,60 @@
 #include "myas.h"
 
-extern int is_valid(char *op, char* argA, char* argB);
+//trans.c
+extern int is_valid(char *op,char* argF,char* argB);
+extern int Check_kind(char *Sen);
 extern char* Reg[8];
 
-int Regtype(char *arg){
-	int i=0;
-	for (i=0;i<8;i++){
-		if(strcmp(arg,Reg[i])==0)
-			return i;
-	}
+int RegCheck(char *regK){
+        int i=0;
+        for (i=0;i<8;i++){
+                if(strcmp(regK,Reg[i])==0)
+                        return i;
+        }
+	return 100;
 }
+
 
 int instr_trans(char *op, char *args, char* mcode)
 {
-	char token[256];
-	int reval;
-	char j=8;
-	char *argx = strtok(token, ",");
-	char *argy = strtok(NULL, ",");
-	char code[10] = "00";
-	strcpy(token, args);
-	
-	printf("%s %s ", argx, argy);
+	char Word[256];
+	char opcode[10] = "00";
+	strcpy(Word,args);
+	char *argF = strtok(Word, ",");
+	char *argB = strtok(NULL, ",");
+	int Kind1 = Check_kind(argF);
+	int Kind2 = Check_kind(argB);
+	char ITR = 8;
 
-	if(!(reval = is_valid(op, argx, argy))){
-		printf("Error: %s %s is not valid\n", op, args);
-		strcpy(mcode, "error");
+	if(is_valid(op,argF,argB)==0){
+		printf("Error: %s %s is not valid\n",op,args);
+		strcpy(mcode,"err");
 		return 0;
 	}
-
-	if(reval == 5)
-		strcpy(code,"89");
-	else if(reval == 6)
-		strcpy(code, "8b");
-	else if(reval == 7)
-		strcpy(code, "a1");
-	else if(reval == 8)
-		strcpy(code, "a3");
-	else if(reval == 9){
-		j = j + Regtype(argy);
-		if(j >= 10){
-			j=j-10;
-			j='a'+j;
-		}
-		else
-			j=j+'0';
-
-		code[0]='b';
-		code[1]=j;
-		code[2]=0;
-	}
 	else{
-		strcpy(code,"error");
+		if((Kind1==4) && (Kind2==4))
+			strcpy(opcode,"89");
+		else if((RegCheck(Kind1)==0) && (Kind2==3))
+			strcpy(opcode,"a3");
+		else if((Kind1==3) && (Kind2==4))
+			strcpy(opcode,"8b");
+		else if((Kind1==3) && (RegCheck(Kind2)==0))
+			strcpy(opcode,"a1");
+		else if((Kind1==2) && (Kind2==4)){
+			ITR += RegCheck(Kind2);
+			if(ITR >= 10)
+				ITR = 'a' + (ITR -10);
+			else
+				ITR = ITR + '0';
+			opcode[0] = 'b';
+			opcode[1] = ITR;
+			opcode[2] = 0;	
+			}
+		else
+			strcpy(opcode,"err");
 	}
-	strcpy(mcode, code);
+	strcpy(mcode, opcode);
+
 	printf("%s\n",mcode);
-	
 	return 1;
 }
