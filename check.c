a78c307c0626016eefb964fd71f3c630fb236745
MunSoo <smboong@naver.com>
2018-11-13 18:51:37 +0900

    practice

--- check.c
+++ check.c
@@ -2,12 +2,13 @@
 
 char* Reg[8] = {"%eax","%ecx","%edx","%ebx","%esp","%ebp","%esi","%edi"};
 char* Mem[8] = {"(%eax)","(%ecx)","(%edx)","(%ebx)","(%esp)","(%ebp)","(%esi)","(%edi)"};
+/*   */
 
 int RegCheck(char *arg) {
-	int i=0:
+	int i=0;
 	for (i=0;i<8;i++){
-		if (strcmp(arg,Reg[i]) == 0
-			return REG;
+		if (strcmp(arg,Reg[i]) == 0)
+			return 2;
 	}
 	return 0;
 }
@@ -15,44 +16,41 @@ int MemCheck(char *arg){
 	int i=0;
 	for (i=0;i<8;i++){
 		if (strstr(arg, Mem[i]) != NULL)
-			return DIS;
+			return 4;
 	}
-	if (arg[0]!='$' && strstr(arg,"0x") ~= NULL)
-		return MEM;
+	if (arg[0]!='$' && strstr(arg,"0x") != NULL)
+		return 3;
 	return 0;
 }
 
 int is_valid(char *op, char *argA, char *argB)
 {
-	if(strcmp(op,"mov")!=0)
-		return 0;
-	int Memout1 = MemCheck(argA);
-	int Memout2 = MemCheck(argB);
-	int Regout1 = RegCheck(argA);
-	int Regout2 = RegCheck(argB);
-	if (Memout1 == DIS){
-		if(RegCheck(argB))
-			return MTR;
-		else
-			return 0;
-	}
-	else if (Memout1 ==MEM){
+	int Memx = MemCheck(argA);
+	int Regx = RegCheck(argA);
+	int Regy = RegCheck(argB);
+	if (Memx == 3){
 		if(strcmp(argB, "%eax") ==0)
-			return MTR_EAX;
+			return 7;
 		else
 			return 0;
 	}
-	else if (Regout1){
+	else if (Memx == 4){
+		if(RegCheck(argB))
+			return 6;
+		else
+			return 0;
+	}
+	else if (Regx){
 		if(strcmp(argA,"%eax") == 0){
-			if (Memcheck(argB))
-				return RTM;
+			if (MemCheck(argB))
+				return 8;
 		}
-		else if (Regout2)
-			return RTR;
+		else if (Regy)
+			return 5;
 	}
 	else if(strstr(argA,"$0x") != NULL){
-		if(Regout2)
-			return ITR;
+		if(Regy)
+			return 9;
 		else
 			return 0;
 	}
--- trans.c
+++ trans.c
@@ -1,12 +1,12 @@
 #include "myas.h"
 
 extern int is_valid(char *op, char* argA, char* argB);
-extern char* reg[8];
+extern char* Reg[8];
 
 int Regtype(char *arg){
 	int i=0;
 	for (i=0;i<8;i++){
-		if(ctrcmp(arg,reg[i])==0)
+		if(strcmp(arg,Reg[i])==0)
 			return i;
 	}
 }
@@ -14,50 +14,46 @@ int Regtype(char *arg){
 int instr_trans(char *op, char *args, char* mcode)
 {
 	char token[256];
-	int i =0;
 	int reval;
-	char j=0;
-	char *argA = strtok(token, ",");
-	char *argB = strtok(NULL, ",");
-	char opcode[10] = "00";
+	char j=8;
+	char *argx = strtok(token, ",");
+	char *argy = strtok(NULL, ",");
+	char code[10] = "00";
 	strcpy(token, args);
 	
-	printf("%s %s ", argA, argB);
+	printf("%s %s ", argx, argy);
 
-	if(!(reval = is_valid(op, argA, argB))){
+	if(!(reval = is_valid(op, argx, argy))){
 		printf("Error: %s %s is not valid\n", op, args);
-		strcpy(mcode, "error";
+		strcpy(mcode, "error");
 		return 0;
 	}
 
-	switch(reval){
-	case RTR:
-		strcpy(opcode,"89");
-		break;
-	case RTM:
-		strcpy(opcode, "a3");
-		break;
-	case MTR:
-		strcpy(opcode, "8b");
-		break;
-	case MTR_EAX:
-		strcpy(opcode, "a1");
-		break;
-	case ITR:
-		j =+ Regtype(argB);
-		if(hex >= 10)
-			j = 'a' + (j -10);
+	if(reval == 5)
+		strcpy(code,"89");
+	else if(reval == 6)
+		strcpy(code, "8b");
+	else if(reval == 7)
+		strcpy(code, "a1");
+	else if(reval == 8)
+		strcpy(code, "a3");
+	else if(reval == 9){
+		j = j + Regtype(argy);
+		if(j >= 10){
+			j=j-10;
+			j='a'+j;
+		}
 		else
-			j = j + '0';
-		opcode[0] = 'b';
-		opcode[1] = j;
-		opcode[2] = 0;
-		break;
-	default:
-		strcpy(opcode, "error");
-	}
-	strcpy(mcode, opcode);
+			j=j+'0';
 
+		code[0]='b';
+		code[1]=j;
+		code[2]=0;
+	}
+	else{
+		strcpy(code,"error");
+	}
+	strcpy(mcode, code);
 	printf("%s\n",mcode);
 	
 	return 1;
