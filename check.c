3a725d9480b2c53c14e52bb47338d36cb6a1c042
MunSoo <smboong@naver.com>
2018-11-13 15:55:39 +0900

    make a new branch

--- check.c
+++ check.c
@@ -1,15 +1,61 @@
 #include "myas.h"
 
-int is_valid(char *op, char *args)
-{
-	//printf("if valid, return 1\n");
-	//printf("otherwise, return 0\n");
+char* Reg[8] = {"%eax","%ecx","%edx","%ebx","%esp","%ebp","%esi","%edi"};
+char* Mem[8] = {"(%eax)","(%ecx)","(%edx)","(%ebx)","(%esp)","(%ebp)","(%esi)","(%edi)"};
 
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	
-	return 1;
+int RegCheck(char *arg) {
+	int i=0:
+	for (i=0;i<8;i++){
+		if (strcmp(arg,Reg[i]) == 0
+			return REG;
+	}
+	return 0;
+}
+int MemCheck(char *arg){
+	int i=0;
+	for (i=0;i<8;i++){
+		if (strstr(arg, Mem[i]) != NULL)
+			return DIS;
+	}
+	if (arg[0]!='$' && strstr(arg,"0x") ~= NULL)
+		return MEM;
+	return 0;
+}
+
+int is_valid(char *op, char *argA, char *argB)
+{
+	if(strcmp(op,"mov")!=0)
+		return 0;
+	int Memout1 = MemCheck(argA);
+	int Memout2 = MemCheck(argB);
+	int Regout1 = RegCheck(argA);
+	int Regout2 = RegCheck(argB);
+	if (Memout1 == DIS){
+		if(RegCheck(argB))
+			return MTR;
+		else
+			return 0;
+	}
+	else if (Memout1 ==MEM){
+		if(strcmp(argB, "%eax") ==0)
+			return MTR_EAX;
+		else
+			return 0;
+	}
+	else if (Regout1){
+		if(strcmp(argA,"%eax") == 0){
+			if (Memcheck(argB))
+				return RTM;
+		}
+		else if (Regout2)
+			return RTR;
+	}
+	else if(strstr(argA,"$0x") != NULL){
+		if(Regout2)
+			return ITR;
+		else
+			return 0;
+	}
+	else
+		return 0;
 }
--- trans.c
+++ trans.c
@@ -1,21 +1,64 @@
 #include "myas.h"
 
+extern int is_valid(char *op, char* argA, char* argB);
+extern char* reg[8];
+
+int Regtype(char *arg){
+	int i=0;
+	for (i=0;i<8;i++){
+		if(ctrcmp(arg,reg[i])==0)
+			return i;
+	}
+}
+
 int instr_trans(char *op, char *args, char* mcode)
 {
-	// check syntax 
-	if(!is_valid(op, args)){
+	char token[256];
+	int i =0;
+	int reval;
+	char j=0;
+	char *argA = strtok(token, ",");
+	char *argB = strtok(NULL, ",");
+	char opcode[10] = "00";
+	strcpy(token, args);
+	
+	printf("%s %s ", argA, argB);
+
+	if(!(reval = is_valid(op, argA, argB))){
 		printf("Error: %s %s is not valid\n", op, args);
+		strcpy(mcode, "error";
 		return 0;
 	}
 
+	switch(reval){
+	case RTR:
+		strcpy(opcode,"89");
+		break;
+	case RTM:
+		strcpy(opcode, "a3");
+		break;
+	case MTR:
+		strcpy(opcode, "8b");
+		break;
+	case MTR_EAX:
+		strcpy(opcode, "a1");
+		break;
+	case ITR:
+		j =+ Regtype(argB);
+		if(hex >= 10)
+			j = 'a' + (j -10);
+		else
+			j = j + '0';
+		opcode[0] = 'b';
+		opcode[1] = j;
+		opcode[2] = 0;
+		break;
+	default:
+		strcpy(opcode, "error");
+	}
+	strcpy(mcode, opcode);
 
-	strcpy(mcode, "AB CD EF");
-
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
+	printf("%s\n",mcode);
 	
-	return 1;	
+	return 1;
 }
